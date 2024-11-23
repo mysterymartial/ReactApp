c9b8ba1b28a6fd6efeb5913434e7db46a103b11e
mysterymartial <bolasax16@gmail.com>
2024-11-23 21:38:42 +0100

    update

---
+++ reduxApp/reduxApp/src/features/counter/counterSlice.jsx
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {value : 0}
+export const counterSlice = createSlice({
+    name: "counter",
+    initialState, // it could be this initialState: {value: 0}
+    reducers:{
+        increment: (state)=>{
+            state.value +=1;  //immer helps us work on this because we are not suppose to mainpulate our state directly
+        },
+        decrement: (state)=>{
+            state.value -= 1;
+        },
+        reset: (state)=>{
+            state.value = 0;
+        }
+    }
+    
+})
+export const [increment, decrement, reset] = counterSlice.actions;
+export default counterSlice.reducer;
